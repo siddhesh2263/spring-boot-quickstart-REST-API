1f1abd5b11282151081109b02b717f3ead671207
siddhesh2263 <siddhesh2263@gmail.com>
2020-07-18 12:40:06 +0530

    first commit

---
+++ src/main/java/io/springbootquickstart/CourseApiJpaDataApplication.java
@@ -0,0 +1,13 @@
+package io.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiJpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiJpaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/springbootquickstart/CourseApiJpaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbootquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiJpaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
